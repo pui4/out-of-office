7e6a1c50ff2dea2257b51a9c9549696fdc923677
pui4 <pitmandonna5@gmail.com>
2025-07-30 20:20:20 +0100

    movement fov

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
-@export var forward_speed : float = 25
-@export var side_speed : float = 5
-@export var max_speed : float = 150
-@export var max_accel : float = 100
+@export var forward_speed : float = 1
+@export var side_speed : float = 1
+@export var max_speed : float = 10
+@export var max_accel : float = 5
 @export var friction : float = 1.1
 @export var max_lean_deg : float = 7
 @export var lean_div : float = 2.5
@@ -15,6 +15,8 @@ extends CharacterBody3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	max_accel *= max_speed
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -29,10 +31,15 @@ func _physics_process(delta: float) -> void:
 	# Movement
 	var wishdir = Vector3(Input.get_axis("left", "right") * side_speed, 0, Input.get_axis("forward", "backward") * forward_speed).normalized()
 	wishdir = wishdir.rotated(Vector3.UP, head.global_rotation.y)
+	
 		
 	var current_speed = velocity.dot(wishdir)
 	var add_speed = clamp(max_speed - current_speed, 0, max_accel * delta)
 	velocity = velocity + add_speed * wishdir
+	
+	camera.fov = remap(velocity.length(), 0, 5, 70, 100)
+	camera.fov = clamp(camera.fov, 70, 100)
+	print(camera.fov)
 	
 	var target_lean_rot = velocity.rotated(Vector3.UP, -camera.global_rotation.y).x / lean_div
 	target_lean_rot = clamp(target_lean_rot, -max_lean_deg, max_lean_deg)
