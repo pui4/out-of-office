fb3f096a470fa4b5a40fd8ded556d2e681710d8f
pui4 <pitmandonna5@gmail.com>
2025-07-31 14:14:33 +0100

    task spawner (if godot has one more engine bug i swear to fucking god im going to go the the godot ceo and shove my dick down his neck)

---
+++ scripts/random_task_spawner.gd
@@ -0,0 +1,12 @@
+extends Marker3D
+
+@export var chance_to_spawn : int = 2
+@export var tasks : Array[String] = ["printer_task", "whiteboard_task"]
+
+func _ready() -> void:
+	var chance : int = randi() % chance_to_spawn - 1
+	if chance == 0:
+		var target_task : int = randi() % len(tasks) - 1
+		var task : PackedScene = load("res://assets/tasks/%s.tscn" % tasks[target_task])
+		var task_inst : Node3D = task.instantiate()
+		add_child(task_inst)
