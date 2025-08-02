541a3a1eebaa20cb7a41c5823dd294c917eda666
pui4 (Daniel) <70605806+pui4@users.noreply.github.com>
2025-08-02 21:58:58 +0100

    main menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -26,3 +26,21 @@ func resume() -> void:
 func _on_button_2_pressed() -> void:
 	settings_inst = settings.instantiate()
 	add_child(settings_inst)
+
+func _on_button_3_pressed() -> void:
+	var menu_scene : PackedScene = load("res://main_menu.tscn")
+	var menu_inst := menu_scene.instantiate()
+	get_tree().root.add_child(menu_inst)
+	
+	Lib.player.queue_free()
+	Lib.current_room.queue_free()
+	Lib.time.stop()
+	Lib.time.wait_time = Lib.base_time
+	Lib.audio_node.stop()
+	Lib.audio_node.stream = Lib.base_ambiance
+	Lib.audio_node.play()
+	Lib.door_count = 0
+	for guard in get_tree().get_nodes_in_group("guard"):
+		guard.queue_free()
+	queue_free()
+	get_tree().paused = false
