c80a7717186109cf5024974145f9df08650ef213
pui4 (Daniel) <70605806+pui4@users.noreply.github.com>
2025-08-13 22:37:10 +0100

    fixed felix bug

--- rooms/intro screen.gd
+++ rooms/intro screen.gd
@@ -4,7 +4,4 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("left") and frame > 0:
 		frame -= 1
 	if Input.is_action_just_pressed("right"):
-		if frame >= 3:
-			$"../../../cutscene props/AnimationPlayer".play("cutscene")
-		else:
-			frame += 1
+		frame += 1
